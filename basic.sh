4fb48f152fe80da7d7c87c888c69f5b7dc3f3758
Nozomi <77535932+Nozom124@users.noreply.github.com>
2021-08-15 18:42:13 +0800

    Create basic.sh

---
+++ basic.sh
@@ -0,0 +1,5 @@
+apt update && apt upgrade -y
+apt install -y vim zip unzip screen tmux git
+apt install -y htop aria2 curl wget tree
+apt install -y fail2ban dnsutils net-tools
+apt autoremove -y && apt autoclean -y
