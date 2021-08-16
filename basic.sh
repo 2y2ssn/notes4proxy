6266001a4575d741711e86cd1fb980cdb700894f
醜人多作怪 <77535932+Nozom124@users.noreply.github.com>
2021-08-16 10:49:26 +0800

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,5 +1,8 @@
 apt update && apt upgrade -y
 apt install -y vim zip unzip screen tmux git
-apt install -y htop aria2 curl wget tree
+apt install -y htop aria2 curl wget tree sudo
 apt install -y fail2ban dnsutils net-tools
 apt autoremove -y && apt autoclean -y
+
+# Edit Timezone
+sudo timedatectl set-timezone Asia/Shanghai
